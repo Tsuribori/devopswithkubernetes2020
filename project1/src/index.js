0441f0c26ab48c0b4d6ba8264c03a9b3ebbdcf53
Tsuribori <tsuribori@tutanota.com>
2020-11-02 23:41:09 +0200

    Exercise 1.06: Project v0.4

--- project1/src/index.js
+++ project1/src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen("8080", () => {
   console.log("Server started in port 8080");
 });
